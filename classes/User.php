39349f184a6594d6370b6978df572372d6aa7885
AndreaMatarrese <matarreseandrea47@gmail.com>
2023-01-11 11:02:30 +0100

    Reconstruct scheme

---
+++ classes/User.php
@@ -0,0 +1,9 @@
+<?php
+
+class User{
+    protected string $name;
+    protected string $address;
+    protected CreditCard $credidcard;
+    protected int $discount;
+    protected array $products = [];
+}
